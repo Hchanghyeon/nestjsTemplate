78138ffc180099c8040927dd5c85e58b4adc9650
changhyeon <changhyeon.h@kakao.com>
2023-02-20 20:40:12 +0900

    Chore: delete test code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,7 +7,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UserController {
   constructor(private readonly userService: UserService) {}
   
-  @Post()
+  @Post("/")
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.userService.createUser(createUserDto);
   }
